fcdce6534b1b1bd15db2566b0a3d4c3bf496b137
Joni Salmi <joni@josalmi.fi>
2018-02-04 02:02:46 +0200

    Add GET /api/persons endpoint with hardcoded values

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+app.get("/api/persons", (req, res, next) => {
+  res.json([
+    { id: 1, name: "Arto Hellas", number: "040-123456" },
+    { id: 2, name: "Martti Tienari", number: "040-123456" },
+    { id: 3, name: "Arto Järvinen", number: "040-123456" },
+    { id: 4, name: "Lea Kutvonen", number: "040-123456" }
+  ]);
 });
 
 const port = 3000;
