0e3b50b288c6b673f632d3888b1bba290513d574
Joni Salmi <joni@josalmi.fi>
2018-02-04 02:14:27 +0200

    Show person count on GET /info

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "pug");
+
+const persons = [
+  { id: 1, name: "Arto Hellas", number: "040-123456" },
+  { id: 2, name: "Martti Tienari", number: "040-123456" },
+  { id: 3, name: "Arto Järvinen", number: "040-123456" },
+  { id: 4, name: "Lea Kutvonen", number: "040-123456" }
+];
+
+app.get("/info", (req, res, next) => {
+  res.render("info", { persons, date: new Date() });
+});
+
 app.get("/api/persons", (req, res, next) => {
-  res.json([
-    { id: 1, name: "Arto Hellas", number: "040-123456" },
-    { id: 2, name: "Martti Tienari", number: "040-123456" },
-    { id: 3, name: "Arto Järvinen", number: "040-123456" },
-    { id: 4, name: "Lea Kutvonen", number: "040-123456" }
-  ]);
+  res.json(persons);
 });
 
 const port = 3000;
